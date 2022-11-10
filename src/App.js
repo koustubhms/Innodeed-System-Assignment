c72c2d959000bc01662b5daefcee5f64dc1aecf6
koustubh satalkar <100868783+koustubhms@users.noreply.github.com>
2022-11-10 17:07:50 +0530

    1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Cars from './Components/Cars';
+
+function App() {
+  return (
+    <div className='block'>
+    <h1>InnoCars</h1>
+    <div className="App">
+
+      <Cars></Cars>
+    </div>
+    </div>
+  );
+}
+
+export default App;
